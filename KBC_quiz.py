5fede1689eae7fa213e9b31642ad2855ca31faf8
Tushar Chandak <119316384+Tushar292004@users.noreply.github.com>
2023-03-15 17:16:25 +0530

    Update and rename KBC_quiz[1].py to KBC_quiz.py

--- KBC_quiz.py
+++ KBC_quiz.py
@@ -1,14 +1,26 @@
+#this is your question set
 dic=["1.what is the capital of india?\na.mumbai\tb.new delhi\tc.kolkata\td.chennai",
       "2.what is the currency of japan?\na.sgd\tb.aud\tc.diahram\td.yen",
       "3.which is the prime no?\na.2\tb.4\tc.6\td.8",
       "4.who is the prime minister of india?\na.modi\tb.amita shah\tc.rajnath singh\td.rahul gandhi"]
-correct = ["b","d","a","a"]
-amount = [1000,2000,3000,4000]
-earning = 0
-for i in range(len(dic)):
-    print(dic[i])
-    answer = input("Enter your answer : ")
 
+#this is your answer set
+correct = ["b","d","a","a"]
+
+#this is you reward set
+amount = [1000,2000,3000,4000]
+
+#this is your initial earnings
+earning = 0
+
+#loop will iterate through each question order by order
+for i in range(len(dic)):
+    print(dic[i])'=
+
+    #users answer
+    answer = input("Enter your answer : ")
+    
+    #condition will whether the answer is correct or not
     if (answer != correct[i]):
         print("Your answer is wrong you lost.")
         if (earning == 0):
